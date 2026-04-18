ae71637a31182f05b6eb3ebf5ac655aad6f26c81
ericoborgonove <borgonove@gmail.com>
2026-04-18 15:32:11 -0400

    feat(initial files): arquivos pós criaçao do projeto

---
+++ src/data/initialTasks.js
@@ -0,0 +1,7 @@
+export const initialTasks = [
+    {id: 1, text: "Instalar o projeto com Vite", done: true},
+    {id: 2, text: "Confifurar o Tailwind CSS", done: true},
+    {id: 3, text: "Adicionar localStorage", done: false},
+    {id: 4, text: "Implementar ediçao de tarefas", done: false},
+    {id: 5, text: "Criar modo escuro", done: false},
+];
